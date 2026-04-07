5c3dbb2fabe92d43b055aeeaa445b8c7a358fee8
FGiuliari <7384983+FGiuliari@users.noreply.github.com>
2026-04-07 16:09:53 +0200

    pass lipread tokens id to model config

--- src/llamafactory/train/sft/workflow.py
+++ src/llamafactory/train/sft/workflow.py
@@ -46,6 +46,11 @@ def run_sft(
     generating_args: "GeneratingArguments",
     callbacks: Optional[list["TrainerCallback"]] = None,
 ):
+    if model_args.use_speechlmm_wrapper:
+        original_add_special_tokens= "" if getattr(model_args,"add_special_tokens",None) is None else model_args.add_special_tokens
+        lipread_tokens=",<|lipread_start|>,<|lipread_pad|>,<|lipread_end|>"
+        model_args.add_special_tokens=original_add_special_tokens+lipread_tokens
+
     tokenizer_module = load_tokenizer(model_args)
     tokenizer = tokenizer_module["tokenizer"]
     template = get_template_and_fix_tokenizer(tokenizer, data_args)
