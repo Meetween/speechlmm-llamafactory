cccf935439e734f66d7c7b3e8fc12b32bafc458a
Thomas-Passera <thomas.passera00gmail.com>
2026-07-16 16:28:23 +0000

    Fix Qwen2-VL language tower freezing

--- src/llamafactory/model/model_utils/visual.py
+++ src/llamafactory/model/model_utils/visual.py
@@ -533,7 +533,7 @@ _register_composite_model(
     model_type="qwen2_vl",
     projector_key="visual.merger",
     vision_model_keys=["visual.patch_embed", "visual.blocks"],
-    language_model_keys=["language_model", "lm_head"]
+    language_model_keys=["model.language_model", "lm_head"]
     if is_transformers_version_greater_than("4.52.0")
     else ["model", "lm_head"],
     lora_conflict_keys=["patch_embed"],
